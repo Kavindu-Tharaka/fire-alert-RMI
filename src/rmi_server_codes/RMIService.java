6d0b59f1d24b6ef315641d10da935dd44a850b3d
Kavindu-Tharaka <43477940+Kavindu-Tharaka@users.noreply.github.com>
2020-10-01 11:39:13 +0530

    Update RMIService.java

--- src/rmi_server_codes/RMIService.java
+++ src/rmi_server_codes/RMIService.java
@@ -3,20 +3,26 @@ package rmi_server_codes;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface RMIService extends Remote {
 
-	/*
-	 * methods that are exposed to desktop client
-	 * */
+/*
+* this interface contains the methods that are exposed to desktop client
+* */
+public interface RMIService extends Remote {
 	
+	
+	//used to retrieve sensor readings and other details of all sensors
 	public String getAllSensorDetails() throws RemoteException;
 
+	//used to retrieve authenticate Admin login credentials
 	public String loginValidator(String email, String password) throws RemoteException;
 
+	//used to register a new sensor
 	public boolean addSensor(String id, int floor, String room) throws RemoteException;
 
+	//used to edit existing sensor details
 	public boolean editSensor(String id, int floor, String room) throws RemoteException;
 
+	//used to delete existing sensor
 	public boolean deleteSensor(String id) throws RemoteException;
 
 }
